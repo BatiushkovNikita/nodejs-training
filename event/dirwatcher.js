3751a0b2003c6cac7de5db480b36a057bd273bf6
Nikita_Batsiushkou <nikita_batsiushkou@epam.com>
2019-03-05 09:57:34 +0300

    fix

--- event/dirwatcher.js
+++ event/dirwatcher.js
@@ -5,27 +5,37 @@ import {promisify} from 'util';
 const readdirAsync = promisify(fs.readdir);
 const statAsync = promisify(fs.stat);
 
+let size = 0;
+
 export default class DirWatcher {
 
     watch(path, delay) {
-        this.detect(path, Date.now());
+        let prevCall = Date.now();
+            this.detect(path, prevCall);
         setInterval(() => {
-            this.detect(path, delay);
+            this.detect(path, prevCall).then(t => prevCall = t);
         }, delay);
     }
 
-    detect(path, delay) {
+    detect(path, prevCall) {
         return readdirAsync(path)
             .then(files => {
                 let endTime = Date.now();
+                let actualSize = files.length;
                 files.forEach(fileName => {
                     let p = path + '/' + fileName;
-                    statAsync(p).then(stats => {
-                        if (stats.ctimeMs <= endTime && stats.ctimeMs > endTime - delay) {
-                            emitter.emit('changed', {path: p});
-                        }
-                    });
+                    if (actualSize !== size) {
+                        emitter.emit('changed', {path: p});
+                    } else {
+                        statAsync(p).then(stats => {
+                            if (stats.ctimeMs <= endTime && stats.ctimeMs > prevCall) {
+                                emitter.emit('changed', {path: p});
+                            }
+                        });
+                    }
                 });
+                size = actualSize;
+                return endTime;
             })
             .catch(console.error);
     }
