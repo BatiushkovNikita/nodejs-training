bba66e8106a861699ec56ef81cd47001d9dc5c26
nbatsiushkou <nbatsiushkou@iba.by>
2019-03-12 00:12:43 +0300

    module4

---
+++ http-servers/html-server..js
@@ -0,0 +1,11 @@
+//import net from 'net';
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost', 10);
+
---
+++ http-servers/plain-text-server.js
@@ -0,0 +1,9 @@
+//import net from 'net';
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost', 10);
